79b25802a66803a208229b29a1296af1d888ad19
Vladimir Kotal <vlada@devnull.cz>
2021-09-17 22:05:46 +0200

    remove unused import

--- src/test/java/org/vladak/TreeNodeTest.java
+++ src/test/java/org/vladak/TreeNodeTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
